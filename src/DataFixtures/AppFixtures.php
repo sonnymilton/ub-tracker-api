90807bb596f70a59abf8fe7ba279328121dc664d
Nikolay Mikhaylov <sonny@milton.pro>
2019-06-30 20:48:43 +0300

    Add user fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\DBAL\Types\BugPriorityType;
 use App\Entity\Bug;
 use App\Entity\Project;
+use App\Entity\Security\ApiUser;
 use App\Entity\Tracker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -21,8 +22,12 @@ class AppFixtures extends Fixture
                 BugPriorityType::getRandomValue(), null));
 
         }
-
         $manager->persist($project);
+
+        $user = new ApiUser('admin', 'admin@example.com', 'admin', ['ROLE_ADMIN']);
+        $user->createToken();
+        $manager->persist($user);
+
         $manager->flush();
     }
 }
