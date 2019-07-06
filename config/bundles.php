7582e1f4cc796d254b0ecba14dcdb92288509cbd
Nikolay Mikhaylov <sonny@milton.pro>
2019-07-06 04:29:18 +0300

    Install KnpUOAuth2ClientBundle

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
