773b957a23c9c137cf18346f970bdf6ec3d60263
Nikolay Mikhaylov <sonny@milton.pro>
2019-07-17 23:45:14 +0300

    Implement ErrorProviderInterface in JsonRequest

--- src/Request/JsonRequest.php
+++ src/Request/JsonRequest.php
@@ -12,14 +12,19 @@
 namespace App\Request;
 
 
+use Fesor\RequestObject\ErrorResponseProvider;
 use Fesor\RequestObject\PayloadResolver;
 use Fesor\RequestObject\RequestObject;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
  * Class JsonRequest
  */
-class JsonRequest extends RequestObject implements PayloadResolver
+class JsonRequest extends RequestObject implements PayloadResolver, ErrorResponseProvider
 {
     /**
      * @param Request $request
@@ -29,5 +34,23 @@ class JsonRequest extends RequestObject implements PayloadResolver
     public function resolvePayload(Request $request): array
     {
         return json_decode($request->getContent(), true) ?? [];
+    }
+
+    /**
+     * @param ConstraintViolationListInterface $errors
+     *
+     * @return Response
+     */
+    public function getErrorResponse(ConstraintViolationListInterface $errors): Response
+    {
+        return new JsonResponse([
+            'message' => 'Please check your data',
+            'errors' => array_map(function (ConstraintViolation $violation) {
+                return [
+                    'path' => $violation->getPropertyPath(),
+                    'message' => $violation->getMessage(),
+                ];
+            }, iterator_to_array($errors)),
+        ], 400);
     }
 }
