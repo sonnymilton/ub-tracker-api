17663bea6b35216d011682c50ddc77f15b10e0b3
Nikolay Mikhaylov <sonny@milton.pro>
2019-11-02 03:46:49 +0300

    Remove bug list from JSON serialized project

--- src/Entity/Tracker.php
+++ src/Entity/Tracker.php
@@ -62,7 +62,7 @@ class Tracker
      * @ORM\OneToMany(targetEntity="Bug", mappedBy="tracker", cascade={"persist", "remove"})
      *
      * @JMS\Expose()
-     * @JMS\Groups(groups={"tracker_list", "tracker_show"})
+     * @JMS\Groups(groups={"tracker_show"})
      */
     protected $bugs;
 
