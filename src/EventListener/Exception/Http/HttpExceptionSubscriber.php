3ffa29751f42c063091b9862241f5e05a2d8324f
Nikolay Mikhaylov <sonny@milton.pro>
2019-12-15 04:35:09 +0300

    Use single hangler for all http exceptions

--- src/EventListener/Exception/Http/HttpExceptionSubscriber.php
+++ src/EventListener/Exception/Http/HttpExceptionSubscriber.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @author      Nickolay Mikhaylov <sonny@milton.pro>
- * @copyright   Copyright (c) 2019, Darvin Studio
- * @link        https://www.darvin-studio.ru
+ * @author    Nickolay Mikhaylov <sonny@milton.pro>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,20 +12,19 @@ namespace App\EventListener\Exception\Http;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
- * Not found HTTP exception subscriber
+ * Http exception subscriber
  */
-class NotFoundHttpExceptionSubscriber implements EventSubscriberInterface
+class HttpExceptionSubscriber implements EventSubscriberInterface
 {
     /**
      * @return array
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             KernelEvents::EXCEPTION => 'onException',
@@ -39,12 +38,12 @@ class NotFoundHttpExceptionSubscriber implements EventSubscriberInterface
     {
         $exception = $event->getThrowable();
 
-        if ($exception instanceof NotFoundHttpException) {
+        if ($exception instanceof HttpExceptionInterface) {
             $event->setResponse(new JsonResponse([
                 'errors' => [
                     $exception->getMessage(),
                 ],
-            ], Response::HTTP_NOT_FOUND));
+            ], $exception->getStatusCode()));
         }
     }
 }
